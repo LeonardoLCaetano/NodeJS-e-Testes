ab73391480c0e325090c5a5ac3888a48497d5017
Leonardo Lopes <leonardo.lopesc03@gmail.com>
2024-05-01 18:10:23 -0300

    Estudando testes com NodeJS

--- calculocombustivel.test.js
+++ calculocombustivel.test.js
@@ -1,7 +1,15 @@
 //IMPORTAÇÃO DA FUNÇÃO
 const calculocombustivel = require("./calculocombustivel")
 
-//TESTE
+//TESTES
 test('Calcular a quantidade em litros de gasolina utilizada para percorrer 150000 metros.', () => {
     expect(calculocombustivel.calcularCombustivel(150000, 16)).toBe(9375)
+})
+
+test('Garantir que o valor não seja nulo.', () => {
+    expect(calculocombustivel.calcularCombustivel(150000, 16)).not.toBeNull()
+})
+
+test('Garantir que seja um número positivo.', () => {
+    expect(calculocombustivel.calcularCombustivel(150000, 16)).toBeGreaterThan(0)
 })
