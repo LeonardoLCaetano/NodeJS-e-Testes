bf9ea3451863e133368cdacf1eaca36eef6176c2
Leonardo Lopes <leonardo.lopesc03@gmail.com>
2024-05-14 20:48:15 -0300

    Leitor de planilhas Google

--- PlanilhaGoogle/index.js
+++ PlanilhaGoogle/index.js
@@ -1,5 +1,5 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const credenciais = require ('./credentials.json');
+const credenciais = require('./credentials.json');
 const arquivo = require('./arquivo.json');
 const { JWT } = require('google-auth-library');
 
@@ -13,48 +13,48 @@ const jwt = new JWT({
     scopes: SCOPES,
 });
 
-async function GetDoc(){
+async function GetDoc() {
     let doc = new GoogleSpreadsheet(arquivo.id, jwt)
     await doc.loadInfo();
     return doc;
 }
 
-async function ReadWorkSheet(){
+async function ReadWorkSheet() {
     let sheet = (await GetDoc()).sheetsByIndex[0];
     let rows = await sheet.getRows();
-    let users = rows.map(row =>{
+    let users = rows.map(row => {
         return row.toObject();
     })
 
     return users;
 }
 
-async function AddUser(data = {}){
+async function AddUser(data = {}) {
     const response = await fetch('https://apigenerator.dronahq.com/api/GMPBvtLv/planilhaGoogleSheets', {
-  
-        method: 'POST', 
-  
+
+        method: 'POST',
+
         headers: {
-    
-          'Content-Type': 'application/json'
-    
+
+            'Content-Type': 'application/json'
+
         },
-  
+
         body: JSON.stringify(data)
-    
-      });
-    
-      return response.json();
-    
+
+    });
+
+    return response.json();
+
 }
 
-async function TrackData(){
+async function TrackData() {
     let data = await ReadWorkSheet();
     data.map(async (user) => {
         let response = await AddUser(user);
         console.log(response);
-        return response; 
-})
+        return response;
+    })
 
     return console.log('Dados copiados com sucesso!')
 }
